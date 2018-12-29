35b232cde44c1ee2f82dba7c6d8aa2cdf508b0a8
samuel.cahyawijaya@gmail.com <samuel.cahyawijaya@gmail.com>
2018-12-30 00:58:20 +0700

    update net.py - change model

--- model/net.py
+++ model/net.py
@@ -7,31 +7,33 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
       
+        self.do2d = nn.Dropout2d(p=0.15)
+        
         self.conv1_1 = nn.Conv2d(1, 8, 3, padding=1)      
-        self.conv1_2 = nn.Conv2d(8, 8, 3, padding=1)      
-        self.conv1_3 = nn.Conv2d(8, 8, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(8, 16, 3, padding=1)      
+        self.conv1_3 = nn.Conv2d(16, 24, 3, padding=1)
         
-        self.pool1 = nn.MaxPool2d(2, 2) 
+        self.pool1 = nn.Conv2d(24, 32, 3, padding=1, stride=2) 
         
-        self.conv2_1 = nn.Conv2d(8, 16, 3, padding=1)      
-        self.conv2_2 = nn.Conv2d(16, 16, 3, padding=1)      
-        self.conv2_3 = nn.Conv2d(16, 16, 3, padding=1)
+        self.conv2_1 = nn.Conv2d(32, 40, 3, padding=1)      
+        self.conv2_2 = nn.Conv2d(40, 48, 3, padding=1)      
+        self.conv2_3 = nn.Conv2d(48, 56, 3, padding=1)
 
-        self.pool2 = nn.MaxPool2d(2, 2) 
+        self.pool2 = nn.Conv2d(56, 64, 3, padding=1, stride=2)
         
-        self.conv3_1 = nn.Conv2d(16, 32, 3, padding=1)      
-        self.conv3_2 = nn.Conv2d(32, 32, 3, padding=1)      
-        self.conv3_3 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv3_1 = nn.Conv2d(64, 72, 3, padding=1)      
+        self.conv3_2 = nn.Conv2d(72, 80, 3, padding=1)      
+        self.conv3_3 = nn.Conv2d(80, 88, 3, padding=1)
         
-        self.pool3 = nn.MaxPool2d(2, 2) 
+        self.pool3 = nn.Conv2d(88, 96, 3, padding=1, stride=2)
         
-        self.conv4_1 = nn.Conv2d(32, 64, 3, padding=1)      
-        self.conv4_2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv4_3 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv4_1 = nn.Conv2d(96, 128, 3, padding=1)      
+        self.conv4_2 = nn.Conv2d(128, 192, 3, padding=1)
+        self.conv4_3 = nn.Conv2d(192, 256, 3, padding=1)
         
-        self.fc1 = nn.Linear(64, 64)
-        self.do = nn.Dropout(p=0.35)
-        self.fc2 = nn.Linear(64, 10)
+        self.fc1 = nn.Linear(256, 256, bias=False)
+        self.do = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(256, 10, bias=False)
         
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -44,26 +46,27 @@ class Net(nn.Module):
         x = F.relu(self.conv1_1(x))
         x = F.relu(self.conv1_2(x))
         x = F.relu(self.conv1_3(x))
-        x = self.pool1(x)
+        x = F.relu(self.pool1(x))
         
         # 32x32x8 => 16x16x16
         x = F.relu(self.conv2_1(x))
         x = F.relu(self.conv2_2(x))
         x = F.relu(self.conv2_3(x))
-        x = self.pool2(x)
+        x = F.relu(self.pool2(x))
         
         # 16x16x16 => 8x8x32
         x = F.relu(self.conv3_1(x))
         x = F.relu(self.conv3_2(x))
         x = F.relu(self.conv3_3(x))
-        x = self.pool3(x)
+        x = self.do2d(F.relu(self.pool3(x)))
         
         # 8x8x32 => 8x8x10
         x = F.relu(self.conv4_1(x))
         x = F.relu(self.conv4_2(x))
         x = F.relu(self.conv4_3(x))
 
-        x = self.do(self.fc1(x.view(-1, 64, 8*8).sum(dim=-1)))
+        x = x.view(-1, 256, 8*8).sum(dim=-1)
+        x = self.do(F.relu(self.fc1(x)))
         x = F.log_softmax(self.fc2(x), dim=-1)
 
         return x
